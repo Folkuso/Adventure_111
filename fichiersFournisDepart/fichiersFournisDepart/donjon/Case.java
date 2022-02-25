1030bf19ae1f6a03ed616c0fbbe8c3cc2107d362
Samuel <svelasco514@gmail.com>
2022-02-25 11:51:14 -0500

    CasePresqueFinie

--- fichiersFournisDepart/fichiersFournisDepart/donjon/Case.java
+++ fichiersFournisDepart/fichiersFournisDepart/donjon/Case.java
@@ -10,12 +10,13 @@ public class Case {
 	private boolean finNiveau;
 	private boolean deAlgoLaby;
 	
-	int voisin[] = new int[4];
+	Case voisin[] = new Case[4];
 	
 	
 	public Case(Position p) {
 		
 		casePosition = new Position(p);
+		
 		
 		
 		
@@ -29,5 +30,40 @@ public class Case {
 		
 		return copiePosition;
 	}
+
+	public boolean estDecouverte() {
+		return decouverte;
+	}
+
+	public void setDecouverte(boolean decouverte) {
+		this.decouverte = decouverte;
+	}
+
+	public boolean estFinNiveau() {
+		return finNiveau;
+	}
+
+	public void setFinNiveau(boolean finNiveau) {
+		this.finNiveau = finNiveau;
+	}
+
+	public boolean estDeAlgoLaby() {
+		return deAlgoLaby;
+	}
+
+	public void setDeAlgoLaby(boolean deAlgoLaby) {
+		this.deAlgoLaby = deAlgoLaby;
+	}
+	
+	public void setVoisin(int direction) {
+		
+		
+		Position voisinP = new Position(this.casePosition);
+		
+		voisinP.additionnerPos(Direction.directionAPosition(direction));
+		voisin[direction].casePosition = voisinP;
+		
+	}
+	
 	
 }
